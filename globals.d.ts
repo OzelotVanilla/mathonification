bc634bfddc5621ab1cc00fa68a4f93724d1df23d
Ozelot Vanilla <ozelotvanilla@gmail.com>
2025-12-04 00:32:01 +0900

    fix - No init text for `syntax_conjunctions_highlight`

--- globals.d.ts
+++ globals.d.ts
@@ -1,4 +1,9 @@
 // Solve the cannot import error of CSS file.
-declare module '*.css';
-declare module '*.scss';
-declare module '*.module.css'; // For CSS Modules
+declare module "*.css";
+declare module "*.scss";
+declare module "*.module.css"; // For CSS Modules
+declare module "*.txt" // To load txt statically
+{
+    const content: string
+    export default content
+}
---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import { NextConfig } from "next";
+
+const next_config = {
+    // reference: https://kuma-emon.com/it/pc/6049/
+    webpack(config, options)
+    {
+        config.module.rules.push({
+            test: /\.(txt)$/,
+            type: "asset/source",
+        });
+
+        return config;
+    },
+} satisfies NextConfig
+
+export default next_config
