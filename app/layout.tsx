1839746ac69632e468c52253c3a026a4c6befcc3
Ozelot Vanilla <ozelotvanilla@gmail.com>
2026-01-01 16:20:22 +0900

    fix - Add dummy favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ export default function RootLayout({ children }: { children: React.ReactNode })
 {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="data:," />
+            </head>
             <body>{children}</body>
         </html>
     )
