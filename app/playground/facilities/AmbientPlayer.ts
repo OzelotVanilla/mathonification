b70390c7e23f38f67049a4595378bc21bfbb70ea
Ozelot Vanilla <ozelotvanilla@gmail.com>
2026-01-02 00:19:07 +0900

    feature - Fade in/out of ambient player when facility select status changes

--- app/playground/facilities/AmbientPlayer.ts
+++ app/playground/facilities/AmbientPlayer.ts
@@ -34,6 +34,35 @@ export abstract class AmbientPlayer
      */
     abstract update(measure: number, beat: number): void
 
+    /**
+     * Ramp master gain value of the player.
+     */
+    abstract linearRampGainTo(value: number, in_n_milliseconds: number): void
+
+    static active_ambient_player__refs: Set<AmbientPlayer> = new Set()
+
+    /**
+     * Let all active ambient player fade-out (turn gain to 0) in n milliseconds.
+     */
+    static fadeOutGain(in_n_milliseconds: number)
+    {
+        for (const player of this.active_ambient_player__refs)
+        {
+            player.linearRampGainTo(0, in_n_milliseconds)
+        }
+    }
+
+    /**
+     * Let all active ambient player fade-in (turn gain to 1) in n milliseconds.
+     */
+    static fadeInGain(in_n_milliseconds: number)
+    {
+        for (const player of this.active_ambient_player__refs)
+        {
+            player.linearRampGainTo(1, in_n_milliseconds)
+        }
+    }
+
     constructor(music_context__ref: MusicContext)
     {
         this.music_context__ref = music_context__ref
